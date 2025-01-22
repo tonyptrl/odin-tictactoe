2f23da77ef2cce8d967c9439ec15683f67753a64
Tony Petrilli <petrilli.tony@gmail.com>
2025-01-22 07:52:23 +0100

    Put gameboard inside an IIFE

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-const gameboard = [
-  ["1", "2", "3"],
-  ["4", "5", "6"],
-  ["7", "8", "9"]
-];
+const gameboard = (function() {
+  return [
+    ["1", "2", "3"],
+    ["4", "5", "6"],
+    ["7", "8", "9"]
+  ];
+})();
 
 function displayGameboard() {
   gameboard.forEach((row) => console.log(row));
