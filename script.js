7122aa1f4c5ead95ed244946ba2f2c98b7ace00a
Tony Petrilli <petrilli.tony@gmail.com>
2025-01-22 08:24:45 +0100

    Add makeMove inside gameController and put players in an IIFE

--- script.js
+++ script.js
@@ -12,13 +12,39 @@ function displayGameboard() {
 
 displayGameboard();
 
-const players = {
-  player1: {
-    name: "Tony",
-    marker: "X"
-  },
-  player2: {
-    name: "Arnaud",
-    marker: "O"
-  }
-};
+const players = (function() {
+  return {
+    player1: {
+      name: "Tony",
+      marker: "X"
+    },
+    player2: {
+      name: "Arnaud",
+      marker: "O"
+    }
+  };
+})();
+
+const gameController = (function() {
+  const makeMove = (position, marker) => {
+    let row = "";
+    let column = "";
+    for (let i = 0; i < gameboard.length; i++) {
+      for (let j = 0; j < gameboard[i].length; j++) {
+        if (gameboard[i][j] === position.toString()) {
+          row = i;
+          column = j;
+        };
+      };
+    };
+    gameboard[row][column] = marker;
+    displayGameboard();
+  };
+
+  return {
+    makeMove,
+  };
+})();
+
+gameController.makeMove("5", "O");
+gameController.makeMove("3", "X");
