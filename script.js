255c56ccd755162862659d94eac6f68bc7724e6b
Tony Petrilli <petrilli.tony@gmail.com>
2025-01-29 08:14:26 +0100

    Make the startGame function working

--- script.js
+++ script.js
@@ -44,14 +44,19 @@ const gameboard = (function() {
 // GameController
 
 const gameController = (function() {
-
   const board = gameboard.getBoard();
 
   let player1Turn = 1;
   let player2Turn = 0;
 
   const startGame = (function() {
-    //console.log(`${players.player1.name}'s Turn`);
+    console.log(`Game started between ${players.player1.name} and ${players.player2.name}!`)
+    console.log(`${players.player1.name}'s Turn (X)`);
+    const player1Move = prompt("What's your move?");
+    gameController.makeMove(player1Move, players.player1.marker);
+    player1Turn = 0;
+    player2Turn = 1;
+    checkWin(players.player1);
   });
 
   const makeMove = (position, marker) => {
@@ -79,14 +84,14 @@ const gameController = (function() {
 
   const handleTurn = (function() {
     if (player1Turn === 1) {
-      console.log(`${players.player1.name}'s Turn`);
+      console.log(`${players.player1.name}'s Turn (X)`);
       const player1Move = prompt("What's your move?");
       gameController.makeMove(player1Move, players.player1.marker);
       player1Turn = 0;
       player2Turn = 1;
       checkWin(players.player1);
     } else {
-        console.log(`${players.player2.name}'s Turn`);
+        console.log(`${players.player2.name}'s Turn (O)`);
         const player2Move = prompt("What's your move?");
         gameController.makeMove(player2Move, players.player2.marker);
         player2Turn = 0;
@@ -96,7 +101,6 @@ const gameController = (function() {
   });
 
   const checkWin = (function(player) {
-
     if(board[0][0] === player.marker
     && board[0][1] === player.marker
     && board[0][2] === player.marker) {
